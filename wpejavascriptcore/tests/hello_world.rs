5bfbd8715f9cd643d223b78a7c37e60bffed046c
i509VCB <git@i509.me>
2022-04-29 23:26:52 -0500

    wpejavasriptcore basic test

---
+++ wpejavascriptcore/tests/hello_world.rs
@@ -0,0 +1,17 @@
+use wpejavascriptcore::{Context, traits::{ContextExt, ValueExt}};
+
+#[test]
+fn forty_two() {
+    let context = Context::new();
+
+    match context.evaluate("42") {
+        Some(value) => {
+            println!("is_boolean: {}", value.is_boolean());
+            println!("is_number: {}", value.is_number());
+            println!("{:?}", value.to_int32());
+            println!("{:?}", value.to_boolean());
+            
+        },
+        None => println!("eval error"),
+    };
+}
